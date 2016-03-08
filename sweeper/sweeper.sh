bfc95a075183755a6f33368d96dd2f5340d808bb
Naoto Umemori <n.umemori@gmail.com>
2016-03-08 23:37:06 +0900

    Change output format to csv

--- sweeper/sweeper.sh
+++ sweeper/sweeper.sh
@@ -21,13 +21,23 @@ for line in `cat ${CSV_FILE_IP_LIST} | grep -v ^#`; do
 done
 
 for src_ip in ${target_ips[@]}; do
+	ping_result_oneline=()
+	num_target=0
 	for dst_ip in ${target_ips[@]}; do
 		src_ip_sed=`echo $src_ip | sed -e "s/\./-/g"`
 		ssh ${mgmt_ips[$src_ip_sed]} "ping $dst_ip -I $src_ip -c 1 -i 1 -W 2 > /dev/null"
 		if [ $? -eq 0 ]
-			then echo -e "${green}icmp reachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
-			else echo -e "${red}icmp unreachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
+			#then echo -e "${green}icmp reachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
+			#else echo -e "${red}icmp unreachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
+			then
+				echo -e "${green}icmp reachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
+				ping_result_oneline[$num_target]="o"
+			else
+				echo -e "${red}icmp unreachable: mgmt_ip=${mgmt_ips[$src_ip_sed]}, src_ip=$src_ip, dst_ip=$dst_ip${nocolor}"
+				ping_result_oneline[$num_target]="x"
 		fi
+		num_target=$(( num_target + 1 ))
 	done
 	echo
+	echo ${ping_result_oneline[@]} | tr -s ' ' ',' >> result.csv
 done
